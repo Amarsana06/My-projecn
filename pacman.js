7b4b60deba0aae95f0843acfc2315ef44eb0009c
Amarsana06 <henzyhenovaamarsana@gmail.com>
2026-04-20 19:37:56 +0800

    Create pacman.js

---
+++ pacman.js
@@ -0,0 +1,8 @@
+//board
+let board;
+const rowCount = 21;
+const columnCount = 19;
+const tileSize = 32;
+const boardWidth = columnCount*tileSize;
+const boardHeight = rowCount*tileSize;
+let context;
